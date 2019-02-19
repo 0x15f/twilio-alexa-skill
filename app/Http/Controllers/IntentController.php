412698b02cc2b9b6290936028be6859ba0520a7a
Jake Casto <jake@lynndigital.com>
2019-02-19 15:27:51 +0000

    It works!

--- app/Http/Controllers/IntentController.php
+++ app/Http/Controllers/IntentController.php
@@ -2,12 +2,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Response;
 
 use Twilio\Rest\Client;
 use Twilio\Twiml;
 
 use Illuminate\Support\Facades\Cache;
+
+use Illuminate\Support\Facades\Log;
 
 class IntentController extends Controller
 {
@@ -15,35 +17,34 @@ class IntentController extends Controller
     {
     	$request_id = $request->input('request.requestId');
     	$intent_name = $request->input('request.type') === 'LaunchRequest' ? 'LaunchRequest' : $request->input('request.intent.name');
+    	file_put_contents(storage_path() . '/json.json', json_encode($request, JSON_PRETTY_PRINT));
 
     	switch($intent_name)
     	{
     		case 'LaunchRequest':
 		    	return response()->json([
 		    		'version' => '1.0',
-		    		'outputSpeech' => [
-		    			'type' => 'PlainText',
-		    			'text' => 'Welcome to the Twilio Alexa assistant. Say send a voice message to start.',
-		    		],
-		    		'shouldEndSession' => true,
+		    		'response' => [
+			    		'outputSpeech' => [
+			    			'type' => 'PlainText',
+			    			'text' => 'Welcome to the Twilio Alexa assistant. Try saying send a voice message to start.',
+			    		],
+			    		'shouldEndSession' => false,
+			    	],
 		    	]);
     		break;
     		case 'SelectIntent':
-    			$phone_number = $request->input('request.intent.slots.number.value');
+    			$phone_number = $request->input('request.intent.slots.PHONE.value');
 
 		    	return response()->json([
 		    		'version' => '1.0',
-		    		'outputSpeech' => [
-		    			'type' => 'PlainText',
-		    			'text' => 'fake',
-		    		],
-		    		'reprompt' => [
-		    			'outputSpeech' => [
-		    				'type' => 'PlainText',
-		    				'text' => 'What message would you like to send?',
-		    			],
-		    		],
-		    		'shouldEndSession' => false,
+		    		'response' => [
+			    		'outputSpeech' => [
+			    			'type' => 'PlainText',
+			    			'text' => 'What message would you like to send?',
+			    		],
+			    		'shouldEndSession' => false,
+			    	],
 		    		'sessionAttributes' => [
 		    			'phoneNumber' => $phone_number,
 		    		],
@@ -53,13 +54,13 @@ class IntentController extends Controller
     			// we grab the auth info from the environment file
     			$account_sid = env('TWILIO_ACCOUNT_SID');
     			$auth_token = env('TWILIO_AUTH_TOKEN');
-    			$from_number = env('TWLIO_NUMBER');
+    			$from_number = env('TWILIO_NUMBER');
 
     			// we grab to `To` number from the session data Alexa sends us
-    			$to_number = $request->input('request.sessionAttributes.phoneNumber');
+    			$to_number = $request->input('session.attributes.phoneNumber');
 
     			// message
-    			$message = $request->input('request.intent.slots.message.value');
+    			$message = $request->input('request.intent.slots.MESSAGE.value');
 
 	   			$client = new Client($account_sid, $auth_token);
 				$call = $client->account->calls->create(  
@@ -67,19 +68,21 @@ class IntentController extends Controller
 				    $from_number,
 				    [
 				    	'url' => route('twilio.callback'),
-				    ],
+				    ]
 				);
 
-				// now cache the call sid and message to say
-				Cache::set($call->sid, $message);
+				// now cache the call sid and message
+				Cache::put($call->sid, $message, 5);
 
 		    	return response()->json([
 		    		'version' => '1.0',
-		    		'outputSpeech' => [
-		    			'type' => 'PlainText',
-		    			'text' => 'Sending ' . $message . ' to ' . $to_number,
-		    		],
-		    		'shouldEndSession' => true,
+		    		'response' => [
+			    		'outputSpeech' => [
+			    			'type' => 'PlainText',
+			    			'text' => 'Sending ' . $message . ' to ' . $to_number,
+			    		],
+			    		'shouldEndSession' => true,
+			    	],
 		    	]);
 			break;
     	}
@@ -88,7 +91,7 @@ class IntentController extends Controller
     public function processCall(Request $request)
     {
     	// get twilio call sid
-    	$id = $_POST['CallSid'];
+    	$id = $_REQUEST['CallSid'];
 
     	$message = Cache::get($id);
 
